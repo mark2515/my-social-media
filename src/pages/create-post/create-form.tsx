2fbb2e289f61f81ddde33f04581ffb0cd7c620fe
Dragon4396 <hekunlong2515@gmail.com>
2023-10-29 12:50:34 -0700

    implement CRUD to firebase database

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,6 +1,10 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../config/firebase"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 interface CreateFormData {
     title: string;
@@ -8,6 +12,10 @@ interface CreateFormData {
 }
 
 export const CreateForm = () => {
+
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
     const schema = yup.object().shape({
         title: yup.string().required("You must add a title."),
         description: yup.string().required("You must add some descriptions.")
@@ -17,8 +25,16 @@ export const CreateForm = () => {
         resolver: yupResolver(schema)
     });
 
-    const onCreatePost = (data: CreateFormData) => {
-        console.log(data);
+    const postsRef = collection(db, "posts");
+
+    const onCreatePost = async (data: CreateFormData) => {
+        await addDoc(postsRef, {
+            ...data,
+            username: user?.displayName,
+            userId: user?.uid
+        });
+
+        navigate("/");
     };
 
     return (
