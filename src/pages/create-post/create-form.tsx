7ceabd6c87fed7a6262ed1851b5f97fe88382f8d
Dragon4396 <hekunlong2515@gmail.com>
2023-10-29 11:19:58 -0700

    add error input prompt

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,11 +1,32 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface CreateFormData {
+    title: string;
+    description: string;
+}
 
 export const CreateForm = () => {
+    const schema = yup.object().shape({
+        title: yup.string().required("You must add a title."),
+        description: yup.string().required("You must add some descriptions.")
+    });
+
+    const { register, handleSubmit, formState:{errors} } = useForm<CreateFormData>({
+        resolver: yupResolver(schema)
+    });
+
+    const onCreatePost = (data: CreateFormData) => {
+        console.log(data);
+    };
+
     return (
-        <form>
-            <input placeholder="Title..." /> 
-            <input placeholder="Description..." /> 
+        <form onSubmit={handleSubmit(onCreatePost)}>
+            <input placeholder="Title..." {...register("title")}/>
+            <p style={{color:"red"}}> {errors.title?.message} </p> 
+            <textarea placeholder="Description..." {...register("description")}/>
+            <p style={{color:"red"}}> {errors.description?.message} </p>  
             <input type="Submit" /> 
         </form>
     );
